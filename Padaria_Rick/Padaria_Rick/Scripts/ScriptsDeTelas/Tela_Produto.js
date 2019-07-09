3a2b2d726a7accb955aaacd05c0de34be3c8596a
Estagiario 030 <stag030@benner.com.br>
2019-07-09 15:43:46 -0300

    filtro em js

--- Padaria_Rick/Padaria_Rick/Scripts/ScriptsDeTelas/Tela_Produto.js
+++ Padaria_Rick/Padaria_Rick/Scripts/ScriptsDeTelas/Tela_Produto.js
@@ -2,4 +2,87 @@
 	$("#Valor").maskMoney({ decimal: ".", thousands: "," });
 });
 
+var campoFiltro = document.querySelector("#filtrar-bebida");
+
+campoFiltro.addEventListener("input", function () {
+    console.log("aqui");
+    var pacientes = document.querySelectorAll(".salgados");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".nomSalgado");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+var campoFiltro = document.querySelector("#filtrar-salgado");
+
+campoFiltro.addEventListener("input", function () {
+    console.log("aqui");
+    var pacientes = document.querySelectorAll(".salgados");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".nomeSalgado");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+var campoFiltro = document.querySelector("#filtrar-doce");
+
+campoFiltro.addEventListener("input", function () {
+    console.log("aqui");
+    var pacientes = document.querySelectorAll(".doces");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".nomeDoce");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+
+
 
